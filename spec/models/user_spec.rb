5497bf015bba3dde3443ec211f4851ed1282159a
nagahiroBass <maibassbluegrass@gmail.com>
2022-04-23 21:23:15 +0900

    fakerの設定とuserモデルテストコードの雛形の作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'user registration' do
+    context '新規登録できるとき' do
+      it '新規登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規投稿できないとき' do
+    end
   end
 end
+
