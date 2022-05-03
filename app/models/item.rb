510349686c3b7086a344c8899eb8d85b9fced2f0
nagahiroBass <maibassbluegrass@gmail.com>
2022-05-04 07:21:24 +0900

    Associationの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Assoiations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :days_to_ship
+  belongs_to :prefecture
+  belongs_to :shipping_charge
 end
