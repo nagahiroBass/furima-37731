171169b84526c26d63beead50b7dcd9268cf2257
nagahiroBass <maibassbluegrass@gmail.com>
2022-05-08 19:14:51 +0900

    商品一覧表示機能の実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -100,7 +100,7 @@ RSpec.describe Item, type: :model do
       it 'item should belong to user' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
