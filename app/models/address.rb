a257e1a019469b205b87bc51fc29397bcbf7e561
nagahiroBass <maibassbluegrass@gmail.com>
2022-05-18 20:33:45 +0900

    order_formの作成とaddressモデルのバリデーションの移動

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
-
-  validates :city, :house_number, presence: true
-  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'はハイフンを入れて半角文字列で入力してください' }
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'はハイフン無しの半角数字で入力してください'}
 end
