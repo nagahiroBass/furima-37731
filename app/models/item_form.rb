43f8fd393292767f8fbd3a80beb0562e3f871466
nagahiroBass <maibassbluegrass@gmail.com>
2022-06-01 18:58:20 +0900

    テストコードの移植

--- app/models/item_form.rb
+++ app/models/item_form.rb
@@ -3,7 +3,7 @@ class ItemForm
 
   attr_accessor :name, :explanation, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, :price, :user_id, :image
 
-  validates :name, :explanation, :image, presence: true
+  validates :name, :explanation, :user_id, :image, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id, :condition_id, :days_to_ship_id, :prefecture_id, :shipping_fee_id,
             numericality: { other_than: 1, message: "can't be blank" }
