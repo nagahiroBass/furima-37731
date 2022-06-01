58e27b6fd8d0195a1640c7aaea8cc5f232539ac7
nagahiroBass <maibassbluegrass@gmail.com>
2022-06-01 21:51:04 +0900

    単数タグ登録機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,6 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   has_one :order
+  has_many :item_tag_relations
+  has_many :tags, through: :item_tag_relations
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  has_many :item_tag_relations
+  has_many :item, through: :item_tag_relations
+  validates :tag_name, uniqueness: true
 end
